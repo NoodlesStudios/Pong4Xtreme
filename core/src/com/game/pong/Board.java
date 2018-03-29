5189d0e7b1b8a5eeb51eb4ad6ef31125c850fe7f
DaPasta <davidandrewbaum@gmail.com>
2018-03-29 16:31:09 -0700

    Add coordinate transformation to Board class

--- core/src/com/game/pong/Board.java
+++ core/src/com/game/pong/Board.java
@@ -1,12 +1,17 @@
 package com.game.pong;
 
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public abstract class Board {
     private Rectangle rect;
 
     public Board(float x, float y, float width, float height) {
         rect = new Rectangle(x, y, width, height);
+    }
+
+    public Vector2 transformCoord(Vector2 vec) {
+        return new Vector2(vec.x + rect.getX(), vec.y + rect.getY());
     }
 
     public abstract void draw();
