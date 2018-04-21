5b0917bfc128c2830c099635ede9346b2a4f57af
DaPasta <davidandrewbaum@gmail.com>
2018-04-21 16:51:59 -0700

    Add score board stuff

--- core/src/com/game/pong/ScoreBoard.java
+++ core/src/com/game/pong/ScoreBoard.java
@@ -1,16 +1,36 @@
 package com.game.pong;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.game.pong.screen.GameScreen;
 
 public class ScoreBoard extends Board {
+    private static final int NUM_SCORES = 4;
 
-    public ScoreBoard(float x, float y, float width, float height) {
+    private GameScreen screen;
+    private int[] scores;
+    private Vector2[] pos;
+
+    public ScoreBoard(float x, float y, float width, float height, GameScreen screen) {
         super(x, y, width, height);
+
+        this.screen = screen;
+        scores = new int[NUM_SCORES];
+        pos = new Vector2[NUM_SCORES];
+        for (int i = 0; i < NUM_SCORES; i++) {
+            pos[i] = new Vector2(30, getHeight() - ((i + 1) * 60));
+        }
     }
 
     @Override
     public void draw(ShapeRenderer renderer) {
-
+        screen.font.setColor(Color.WHITE);
+        for(int i = 0; i < NUM_SCORES; i++) {
+            Vector2 scorePos = transformCoord(pos[i]);
+            screen.font.draw(screen.batch, "Player " + (i + 1), scorePos.x, scorePos.y);
+            screen.font.draw(screen.batch, "" + scores[i], scorePos.x, scorePos.y - 30);
+        }
     }
 
     @Override
--- core/src/com/game/pong/screen/GameScreen.java
+++ core/src/com/game/pong/screen/GameScreen.java
@@ -2,21 +2,31 @@ package com.game.pong.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.game.pong.GameBoard;
+import com.game.pong.ScoreBoard;
 
 public class GameScreen implements Screen {
 
     ShapeRenderer renderer;
+    public SpriteBatch batch;
+    public BitmapFont font;
 
     GameBoard gameBoard;
+    ScoreBoard scoreBoard;
 
 
     public GameScreen() {
         renderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+        font = new BitmapFont();
 
         gameBoard = new GameBoard(0, 0, 600);
+        scoreBoard = new ScoreBoard(600, 0, 200, 600, this);
     }
 
     @Override
@@ -26,11 +36,18 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         gameBoard.update();
+        scoreBoard.update();
 
         Gdx.gl20.glClearColor(0, 0, 0, 0);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+
+
         gameBoard.draw(renderer);
+
+        batch.begin();
+        scoreBoard.draw(renderer);
+        batch.end();
     }
 
     @Override
@@ -56,5 +73,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         renderer.dispose();
+        batch.dispose();
+        font.dispose();
     }
 }
