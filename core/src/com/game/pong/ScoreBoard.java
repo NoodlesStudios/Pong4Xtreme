bfd2841ecca2f744c6a01d0bcc656a04df035ade
DaPasta <davidandrewbaum@gmail.com>
2018-04-21 14:44:12 -0700

    Improve input handling for paddles. Add ScoreBoard class.

--- core/src/com/game/pong/ScoreBoard.java
+++ core/src/com/game/pong/ScoreBoard.java
@@ -1,4 +1,20 @@
 package com.game.pong;
 
-public class ScoreBoard {
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class ScoreBoard extends Board {
+
+    public ScoreBoard(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void draw(ShapeRenderer renderer) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
