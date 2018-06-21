322912694acec7632292d74f6f1951d9e68b4f22
DaPasta <davidandrewbaum@gmail.com>
2018-06-21 13:51:28 -0700

    Add beginnings of network code

---
+++ core/src/com/game/pong/network/Client.java
@@ -0,0 +1,43 @@
+package com.game.pong.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+public class Client {
+    private static final String ADDRESS = "127.0.0.1";
+    private static final int PORT = 8080;
+    private static final int BUFFER_CAPACITY = 48;
+
+    DatagramChannel channel;
+
+    public Client() {
+
+    }
+
+    public boolean connect() throws IOException {
+        channel = DatagramChannel.open();
+        //noinspection Since15
+        channel.bind(new InetSocketAddress(ADDRESS, PORT));
+        channel.connect(new InetSocketAddress(ADDRESS, PORT));
+        return isConnected();
+    }
+
+    public boolean isConnected() {
+        return channel.isConnected();
+    }
+
+    public Packet receivePacket() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_CAPACITY);
+        buffer.clear();
+
+        channel.receive(buffer);
+
+        return new Packet();
+    }
+
+    public Packet sendPacket(Packet packet) {
+        
+    }
+}
---
+++ core/src/com/game/pong/network/Packet.java
@@ -0,0 +1,4 @@
+package com.game.pong.network;
+
+public class Packet {
+}
